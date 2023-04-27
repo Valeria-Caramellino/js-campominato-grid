c2ae70adeb619642f38a27a36e0bba7afb13782d
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-27 14:43:53 +0200

    completamento prima parte esercizio

--- js/main.js
+++ js/main.js
@@ -1,32 +1,52 @@
 console.log("ciao");
 //creo una costante per indicare il mio contenitore
 const contentitore = document.getElementById("container");
-
-//creo una funzione per generare le caselle con all'interno il numero
-function GeneraCaselleNumerate (numeroDaGenerare){
-
-    for (let i = 1; i <= numeroDaGenerare; i++) {
-    const newBox = document.createElement("div");
-    newBox.classList.add("miniBox100");
-    newBox.innerText = `${i}`;
-    contentitore.append(newBox);
-    newBox.addEventListener("click", function() {
-        console.log(i);
-        this.classList.toggle("bgNuovo");
-    });
-    }
-}
 //creo collegamento con il pulsante
 const buttonPlay = document.getElementById("play");
 
 //genero evento per il pulsante richiamando la funzione
-
 buttonPlay.addEventListener("click", function(){
-    GeneraCaselleNumerate(100);
+
+    GeneraCaselleNumerate(document.getElementById("livelloDifficolta").value);
 
 })
 
-//due problemi 
-//1 ogni volta che clicco sul bottone mi genera una nuova tabella sotto
-//come faccio a cambiare la misura del minibox??
-//se da 100 diventano 80 non occupa tutta la dimensione del contenitore
+//creo variabile di inizializzazione
+var inizioGame = false;
+//creo una funzione per generare le caselle con all'interno il numero
+function GeneraCaselleNumerate (numeroDaGenerare){
+    if(inizioGame === false){
+        let numeroDiCelleTotali = document.getElementById("livelloDifficolta").value;
+        let numeroDiCellePerLato = Math.ceil( Math.sqrt(numeroDiCelleTotali) );
+        let dimensione = `calc(100% / ${numeroDiCellePerLato})`;
+        for (let i = 1; i <= numeroDaGenerare; i++) {
+            
+            let box= createElement("div" ,"miniBox100" , i );
+            box.style.width = dimensione;
+            box.style.height = dimensione;
+            
+            contentitore.append(box);
+
+            box.addEventListener("click", function() {
+            console.log(i);
+            this.classList.toggle("bgNuovo");
+            });
+        } 
+        inizioGame=true;
+    }else{
+        console.log("il gioco è gia iniziato!");
+    } 
+}
+
+
+
+//funzione di creazione elemento
+function createElement(tagHtml, classe, contenuto) {
+    
+    //Creiamo un elemento
+    const newBox = document.createElement(tagHtml);
+    newBox.classList.add(classe);
+    newBox.innerText = contenuto;
+
+    return newBox;
+}
